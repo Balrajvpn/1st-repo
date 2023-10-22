d9140d8cc00ec13c2519dc4b4dc07dbddd0c0547
Balrajvpn <148621586+Balrajvpn@users.noreply.github.com>
2023-10-22 07:17:22 +0000

    Type conversion with operations

---
+++ .devcontainer/firstFolder/03_Conversiontypeof.js
@@ -0,0 +1,21 @@
+// Type conversion is used to convert one datatype of to another
+//  e.g.
+  let number = 123 
+  console.log(number)
+  console.log(typeof(number))
+  // before type conversion number= 123 is a integer
+
+  let afterNumber = String(number)
+  console.log(afterNumber)
+  console.log(typeof(afterNumber))
+  // after type conversion it will become string
+
+ // we can convert all datatype into another type.
+//  e.g.
+// boolean = integer vise versa
+// integer = string vise versa 
+
+// --------------- Operations ------------------------
+// Arithmetic , logic , bitwise , relational etc.
+// while writing long expression e.g. (2 + 3 - 6 * 7 / 8) we should avoid writing like this 
+// use pernthesis when expression is to long e.g. ((2+3) - (6*7) / 8)
