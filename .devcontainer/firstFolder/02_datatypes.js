694deb7ea57ab1f12f36e0ce5e0052917dfc107e
Balrajvpn <148621586+Balrajvpn@users.noreply.github.com>
2023-10-22 06:37:51 +0000

    02_datatypes

---
+++ .devcontainer/firstFolder/02_datatypes.js
@@ -0,0 +1,18 @@
+// we usully use ECMA standard to see documentation
+// In javascript there are many datatypes but the mainly used are
+
+// number => to declare integer number 
+// bigint => to declare big integer
+// boolean => to specify true of false TRUE/FALSE
+// null => standalone value or object
+// symbol => unique 
+// object => object of class
+
+let number = 1234
+let isok = true
+console.log(number)
+console.log(isok)
+
+// to check data type we use typeof function
+console.log(typeof undefined);
+console.log(typeof null)
