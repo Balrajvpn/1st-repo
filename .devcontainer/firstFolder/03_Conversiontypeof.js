c4a1e840982fa836c0eb7a1130514f792e3784ef
Balrajvpn <148621586+Balrajvpn@users.noreply.github.com>
2023-10-22 07:31:24 +0000

    Conversiontype of + triple equal===

--- .devcontainer/firstFolder/03_Conversiontypeof.js
+++ .devcontainer/firstFolder/03_Conversiontypeof.js
@@ -16,6 +16,16 @@
 // integer = string vise versa 
 
 // --------------- Operations ------------------------
-// Arithmetic , logic , bitwise , relational etc.
+// Arithmetic , logic , bitwise , relational , comperision etc.
 // while writing long expression e.g. (2 + 3 - 6 * 7 / 8) we should avoid writing like this 
 // use pernthesis when expression is to long e.g. ((2+3) - (6*7) / 8)
+
+// === triple equl is used to check strictly means it doesnot convet automatically values in same datatype 
+// e.g. 
+   console.log("2" == 2)
+   // it will convert "2" into number automatically But
+
+console.log("2" === 2)
+    // triple check did not convert automatically it check values strictly
+
+
